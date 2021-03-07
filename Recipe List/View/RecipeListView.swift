abeca0861b99854f4dfa685631ea6b00c2c3a74c
Jon Payne <pilotguy54@gmail.com>
2021-03-07 11:29:30 -0500

    Update from Original Solution Project

--- Recipe List/View/RecipeListView.swift
+++ Recipe List/View/RecipeListView.swift
@@ -9,26 +9,36 @@ import SwiftUI
 
 struct RecipeListView: View {
     
+    // Reference the view model
     @ObservedObject var model = RecipeModel()
     
     var body: some View {
         
-        List(model.recipes) { r in
-            
-            HStack(spacing: 20.0) {
-                Image(r.image)
-                    .resizable()
-                    .scaledToFill()
-                    .frame(width:50, height:50, alignment: .center)
-                    .clipped()
-                    .cornerRadius(5.0)
-                Text(r.name)
+        NavigationView {
+            List(model.recipes) { r in
+                
+                NavigationLink(
+                    destination: RecipeDetailView(recipe:r),
+                    label: {
+                        
+                        // MARK: Row item
+                        HStack(spacing: 20.0) {
+                            Image(r.image)
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 50, height: 50, alignment: .center)
+                                .clipped()
+                                .cornerRadius(5)
+                            Text(r.name)
+                        }
+                        
+                    })
+                
                 
                 
             }
-            
+            .navigationBarTitle("All Recipes")
         }
-        
     }
 }
 
--- Recipe List/ViewModel/RecipeModelView.swift
+++ Recipe List/ViewModel/RecipeModelView.swift
@@ -8,10 +8,14 @@
 import Foundation
 
 class RecipeModel: ObservableObject {
+    
     @Published var recipes = [Recipe]()
     
     init() {
         
+        // Create an instance of data service and get the data
         self.recipes = DataService.getLocalData()
+        
+        
     }
 }
